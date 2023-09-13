5e2d7a17f3f1f7746ad782d16fe86d352e9d7d0b
Stavinskaya_M <marina.st6277@gmail.com>
2023-09-13 15:48:06 +0600

    Allure

--- src/test/java/ru/netology/DataGeneratorTest.java
+++ src/test/java/ru/netology/DataGeneratorTest.java
@@ -1,5 +1,9 @@
 package ru.netology;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +15,17 @@ import static com.codeborne.selenide.Selenide.open;
 public class DataGeneratorTest {
 
     DataGenerator plan = new DataGenerator();
+
+    @BeforeAll
+    static void setUpAll() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void createUrl() {
